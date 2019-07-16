2d89af54a22a491888c3794cb0aac3cc90b0860a
debamala <debamala.dey@indusnet.co.in>
2019-07-16 14:17:52 +0530

    server link updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,9 @@ export class AppComponent {
 
     this.oauthService.redirectUri = window.location.origin;
 
-    this.oauthService.clientId = 'YOUR_CLIENT_ID';
-    this.oauthService.loginUrl = 'https://YOUR_SERVER/adfs/oauth2/authorize';
-    this.oauthService.issuer = 'https://YOUR_SERVER/adfs';
+    this.oauthService.clientId = '73a11f91-517d-4bf1-8e1b-885218d0d7f1';
+    this.oauthService.loginUrl = 'https://logintestappmern.herokuapp.com/adfs/oauth2/authorize';
+    this.oauthService.issuer = 'https://logintestappmern.herokuapp.com/adfs';
 
     this.oauthService.scope = "openid profile";
     this.oauthService.responseType = 'id_token token';
